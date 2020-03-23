aad757e14e1c38baa5e2feeff04eda3735e98d29
Akash Desai <akashdesai2007@gmail.com>
2020-03-23 17:08:05 -0400

    util/nurand.py: Fix rand.number(n1, n2) with random.randrange(n1, n2) for Python3
    Signed-off-by: Akash Desai <akashdesai2007@gmail.com>

--- pytpcc/util/nurand.py
+++ pytpcc/util/nurand.py
@@ -29,13 +29,13 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 # -----------------------------------------------------------------------
 
-import rand
+import random
 
 def makeForLoad():
     """Create random NURand constants, appropriate for loading the database."""
-    cLast = rand.number(0, 255)
-    cId = rand.number(0, 1023)
-    orderLineItemId = rand.number(0, 8191)
+    cLast = random.randrange(0, 255)
+    cId = random.randrange(0, 1023)
+    orderLineItemId = random.randrange(0, 8191)
     return NURandC(cLast, cId, orderLineItemId)
 
 def validCRun(cRun, cLoad):
@@ -45,13 +45,13 @@ def validCRun(cRun, cLoad):
 
 def makeForRun(loadC):
     """Create random NURand constants for running TPC-C. TPC-C 2.1.6.1. (page 20) specifies the valid range for these constants."""
-    cRun = rand.number(0, 255)
+    cRun = random.randrange(0, 255)
     while validCRun(cRun, loadC.cLast) == False:
-        cRun = rand.number(0, 255)
+        cRun = random.randrange(0, 255)
     assert validCRun(cRun, loadC.cLast)
     
-    cId = rand.number(0, 1023)
-    orderLineItemId = rand.number(0, 8191)
+    cId = random.randrange(0, 1023)
+    orderLineItemId = random.randrange(0, 8191)
     return NURandC(cRun, cId, orderLineItemId)
 
 class NURandC:
